064ffcee00392879019dcf19603754c48e8a640b
Mini-Pascal <111233913+Mini-Pascal@users.noreply.github.com>
2023-02-04 13:49:53 +0000

    try

--- AncestryAdventure/Assets/PScript.cs
+++ AncestryAdventure/Assets/PScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
  
-public class PlayerController : MonoBehaviour
+public class PScript : MonoBehaviour
 {
     public Rigidbody2D theRB;
     public float moveSpeed;
